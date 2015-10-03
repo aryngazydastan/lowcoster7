223c3cad5289d9c21162e89f64863006d1430bd0
Dastan Aryngazy <aryngazy.dastan@gmail.com>
2015-10-03 06:02:46 +0600

    Update all

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Lowcoster7
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.assets.initialize_on_precompile = false
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.initialize_on_precompile = false
+    # config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
